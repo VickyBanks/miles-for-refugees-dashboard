06c09627b31a32313c7649bad3950081dbc2f414
Vicky Banks <victoria.banks052@gmail.com>
2020-09-07 21:54:22 +0100

    Added functionality to wrtie into an S3 bucket and then into Redshift
    Needs to be run on the R server

--- fetch.R
+++ fetch.R
@@ -1,6 +1,7 @@
 # Idea 1. Fetch the page for me as a user, and then use regex or somehting else on the file that gets returned
 library(httr)
 library(dplyr)
+source("/data/functions/write_to_redshift.R") #this allows you to write to an S3 bucket and pull into Redshift
 
 # Use the API.
 # # by inspecting the page we've found my user id
@@ -20,8 +21,8 @@ teamUuids<- data.frame(
 ############    getting data ############ 
 #1. find user's uuid and put them all into df
 #2. for each user, 
-  #2.a GET their page and select the useful information
-  #2.b Write this into a DF
+#2.a GET their page and select the useful information
+#2.b Write this into a DF
 #3. write df to csv
 
 
@@ -50,3 +51,7 @@ teamInfo <- getTeamInfo(teamUuids)
 
 #write to csv
 write.csv2(teamInfo, file = "teamInfo.csv", row.names = FALSE)
+
+
+#Write to redshift
+write_to_redshift(df = teamInfo, s3_folder = "vicky_banks", redshift_location = "dataforce_sandbox.vb_miles_refugees_team_info")
